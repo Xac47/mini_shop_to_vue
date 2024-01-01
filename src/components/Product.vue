fd8c6d8159562bdf4bd79316ea940c1290665aa1
Xac47 <hasbulat69@gmail.com>
2024-01-01 22:45:12 +0300

    add component src/components/Product.vue

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,17 +1,30 @@
+<script setup>
+defineProps({
+  title: String,
+  imageUrl: String,
+  price: Number,
+  isAdded: Boolean,
+  isFavorite: Boolean
+})
+</script>
+
 <template>
   <div
-    class="block-item rounded-3xl border-2 border-gray-100 p-4 relative cursor-pointer hover:-translate-y-2 hover:shadow-xl transition duration-300"
-  >
-    <img src="/like-1.svg" alt="like" class="w-10 h-10 absolute top-12 left-4" />
-    <img src="/sneakers/sneakers-1.jpg" alt="product" class="w-120 h-120" />
-    <p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+    class="block-item rounded-3xl border-2 border-gray-100 p-4 relative cursor-pointer hover:-translate-y-2 hover:shadow-xl transition duration-300">
+    <img
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+      alt="like"
+      class="w-10 h-10 absolute top-12 left-4"
+    />
+    <img :src="imageUrl" alt="product" class="w-120 h-120" />
+    <p class="mt-2">{{ title }}</p>
 
     <div class="block-item-wrapper flex justify-between mt-4">
       <div class="block-item-wrapper-info flex flex-col">
         <span class="text-slate-500">Цена:</span>
-        <b>35 000 руб</b>
+        <b>{{ price }} руб</b>
       </div>
-      <img src="/plus.svg" alt="plus" />
+      <img :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="plus" />
     </div>
   </div>
 </template>
