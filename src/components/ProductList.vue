1e02e6a41a91129dd3961140f16519c6a7c9f147
Xac47 <hasbulat69@gmail.com>
2024-01-01 22:44:48 +0300

    add components src/components/ProductList.vue

---
+++ src/components/ProductList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Product from './Product.vue'
+</script>
+
+<template>
+  <div class="block-main-end mt-10 grid grid-cols-4 flex gap-10">
+    <Product imageUrl="/sneakers/sneakers-1.jpg" title="Кроссовки" price="2500" :isAdded="false" :isFavorite="true"/>
+    <Product imageUrl="/sneakers/sneakers-2.jpg" title="Кроссовки" price="2500" :isAdded="false" :isFavorite="true"/>
+    <Product imageUrl="/sneakers/sneakers-3.jpg" title="Кроссовки" price="2500" :isAdded="true" :isFavorite="false"/>
+    <Product imageUrl="/sneakers/sneakers-4.jpg" title="Кроссовки" price="2500" :isAdded="false" :isFavorite="false"/>
+    <Product imageUrl="/sneakers/sneakers-5.jpg" title="Кроссовки" price="2500" :isAdded="true" :isFavorite="true"/>
+    <Product imageUrl="/sneakers/sneakers-6.jpg" title="Кроссовки" price="2500" :isAdded="false" :isFavorite="true"/>
+    <Product imageUrl="/sneakers/sneakers-7.jpg" title="Кроссовки" price="2500" :isAdded="true" :isFavorite="true"/>
+    <Product imageUrl="/sneakers/sneakers-8.jpg" title="Кроссовки" price="2500" :isAdded="true" :isFavorite="false"/>
+  </div>
+</template>
