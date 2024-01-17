ab268f05b72fa2bdfcd6e1151ba3fc99d87e5216
Xac47 <hasbulat69@gmail.com>
2024-01-17 13:24:35 +0300

    create component src/components/Drawer.vue

---
+++ src/components/Drawer.vue
@@ -0,0 +1,18 @@
+<script setup>
+import DrawerHead from './DrawerHead.vue'
+import DrawerEnd from './DrawerEnd.vue';
+import CartItemList from './CartItemList.vue'
+</script>
+
+<template>
+  <div class="fixed top-0 left-0 w-full h-full bg-black z-10 opacity-50"></div>
+  <div class="bg-white w-1/4 h-full fixed top-0 right-0 z-20 p-10 flex flex-col justify-between">
+    <div>
+      <DrawerHead />
+      <CartItemList />
+    </div>
+
+    <DrawerEnd />
+
+  </div>
+</template>
