31e8d97fd814701a2975d7a4959bb0298a9d554a
Xac47 <hasbulat69@gmail.com>
2024-01-17 13:24:11 +0300

    create component src/components/CartItemList.vue

---
+++ src/components/CartItemList.vue
@@ -0,0 +1,13 @@
+<script setup>
+import CartItem from './CartItem.vue'
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <CartItem title="Кроссовки" price="1000" imageUrl="sneakers/sneakers-1.jpg" />
+    <CartItem title="Кроссовки" price="1000" imageUrl="sneakers/sneakers-2.jpg" />
+    <CartItem title="Кроссовки" price="1000" imageUrl="sneakers/sneakers-3.jpg" />
+    <CartItem title="Кроссовки" price="1000" imageUrl="sneakers/sneakers-4.jpg" />
+    <CartItem title="Кроссовки" price="1000" imageUrl="sneakers/sneakers-5.jpg" />
+  </div>
+</template>
